3fad018edab3eeaf22c9517fc9e83449d34678aa
Jessica Man <psjm3@cam.ac.uk>
2023-10-01 16:25:10 +0100

    exit if an invalid option is provided

--- src/chericat.c
+++ src/chericat.c
@@ -20,13 +20,12 @@
 
 static void usage()
 {
-	fprintf(stdout, "\nUsage: chericat [[-d <database name>] [-s <pid>] [-v] [-n <binary name>] [-m]]\n"
+	fprintf(stdout, "\nUsage: chericat [[-d <database name>] [-s <pid>] [-v] [-m <binary name>]]\n"
 			"     pid - pid of the target process for a snapshot of caps info\n"
-			"     database name - name of the database to store data captured by chericat\n"
+			"     database name - name of the database to store data captured by chericat. If omitted, an in-memory db is used\n"
 			"Options:\n"
 			"     -s Scan the mapped memory and persist the caps data to a database\n"
 			"     -v Show summary info of capabilities in the target process, arranged in mmap order\n"
-			//"     -n Show capabilities in the loaded binary <binary name>\n"
 			"     -m Show capabilities with corresponding symbols\n\n");
 }
 
@@ -34,8 +33,7 @@ static struct option long_options[] =
 {
 	{"scan_mem", required_argument, 0, 's'},
 	{"caps_summary", no_argument, 0, 'v'},
-	//{"caps_for_lib", required_argument, 0, 'n'},
-	{"symbols_summary", no_argument, 0, 'm'},
+	{"symbols_summary", required_argument, 0, 'm'},
 	{"database_name", required_argument, 0, 'd'},
 	{0,0,0,0}
 };
@@ -60,7 +58,6 @@ main(int argc, char *argv[])
 	xo_handle_t *xo_handle = xo_create_to_file(fptr, XO_STYLE_JSON, XOF_WARN|XOF_PRETTY);
 
 	int optindex;
-	//int opt = getopt_long(argc, argv, "d:s:vn:m:", long_options, &optindex);
 	int opt = getopt_long(argc, argv, "d:s:vm:", long_options, &optindex);
 
 	if (opt == -1) {
@@ -83,11 +80,7 @@ main(int argc, char *argv[])
                		 		return (1);
         			}
 			}
-
-			//scan_mem(db, optarg);
 			scan_mem_using_procstat(db, optarg);
-			//read_lwps(optarg);
-			//get_vmmap_from_procstat(optarg);
 			break;
 		case 'v':
 			if (db == NULL) {
@@ -102,9 +95,6 @@ main(int argc, char *argv[])
 			xo_open_list("vm_cap_output");
 			vm_caps_view(db);
 			xo_close_list("vm_cap_output");
-			break;
-		case 'n':
-			printf("View of caps within address range of library %s\n", optarg);
 			break;
 		case 'm':
 			if (db == NULL) {
@@ -124,8 +114,8 @@ main(int argc, char *argv[])
 			break;
 		default:
 			usage();
+			exit(0);
 		}
-		//opt = getopt_long(argc, argv, "d:s:vn:m:", long_options, &optindex);
 		opt = getopt_long(argc, argv, "d:s:vm:", long_options, &optindex);
 	}
 
